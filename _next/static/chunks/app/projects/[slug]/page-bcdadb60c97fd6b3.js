a3ab09303d6eb85bfb3fd8985e02594936f2beb5
cole <scshafe@umich.edu>
2026-07-28 15:27:42 -0700

    Update static export 2026-07-28T22:27:42.000Z

---
+++ _next/static/chunks/app/projects/[slug]/page-bcdadb60c97fd6b3.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[101,895],{8807:function(n,e,t){Promise.resolve().then(t.t.bind(t,7400,23)),Promise.resolve().then(t.t.bind(t,231,23))},7400:function(){}},function(n){n.O(0,[46,231,971,23,744],function(){return n(n.s=8807)}),_N_E=n.O()}]);
